c75df15f2065aab83b1f19621b1a0ccef4963c73
Shashank-09 <shashanktulsikar979@gmail.com>
2023-08-16 23:24:48 +0530

    third commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,28 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+import '../globals.css';
+
 export const metadata = {
     title: 'Threads',
     description: 'A Next.js 13 Meta Threads Application'
 }
 
-export default function RootLayout({ childern 
+const inter = Inter({ subsets: ["latin"] })
+
+export default function RootLayout({ 
+    children 
 } : { 
-    childern: React.ReactNode
+    children: React.ReactNode
 }) {
-    return 
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1`}>
+                   {children}
+                </body>
+            </html>
+        </ClerkProvider>
+        
+    )
 
 }
