44ca7374cfb0ddfc077f13f50adb7ae6cd40b7d7
Shashank-09 <shashanktulsikar979@gmail.com>
2023-09-13 23:32:26 +0530

    tenth commit

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -16,8 +16,13 @@ import {
 import { Input } from "@/components/ui/input"
 import { Button } from "../ui/button";
 import Image from "next/image";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from '@/lib/uploadthing'
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname , useRouter} from "next/navigation";
+import path from "path";
 
 interface Props {
   user:{
@@ -35,6 +40,11 @@ interface Props {
 
 const AccountProfile = ({ user , btnTitle }: 
 Props) => {
+
+const [files, setFiles] = useState<File[]>([])
+const { startUpload } = useUploadThing("media")
+const router = useRouter();
+const pathname = usePathname();
  
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -47,14 +57,57 @@ Props) => {
     })
 
 
-  const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+  const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
      e.preventDefault();
+     
+     const fileReader = new FileReader();
+
+     if(e.target.files && e.target.files.length > 0) {
+        const file = e.target.files[0];
+ 
+        setFiles(Array.from(e.target.files));
+
+        if(!file.type.includes('image')) return;
+
+        fileReader.onload = async (event) => {
+            const imageDataUrl = event.target?.result?.toString() || '';
+
+            fieldChange(imageDataUrl);
+        }
+
+        fileReader.readAsDataURL(file)
+
+     }
+
   }
 
-    function onSubmit(values: z.infer<typeof UserValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+  const onSubmit = async (values: z.infer<typeof UserValidation>)  => {
+        const blob = values.profile_photo;
+
+        const hasImageChanged = isBase64Image(blob);
+
+        if(hasImageChanged){
+            const imgRes = await startUpload(files)
+
+            if(imgRes && imgRes[0].fileUrl) {
+                values.profile_photo = imgRes[0].fileUrl;
+            }
+        }
+
+        await updateUser({
+           userId: user.id,
+           username: values.username,
+           name: values.name,
+           bio: values.bio,
+           image: values.profile_photo,
+           path: pathname
+        });
+
+        if(pathname === '/profile/edit'){
+          router.back();
+        }else{
+          router.push('/');
+        }
       }
 
     return(
